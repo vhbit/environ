905bc603a52619217631fcb130c80df05bc6cf8c
Valerii Hiora <valerii.hiora@gmail.com>
2013-05-17 20:21:34 +0300

    Subconfig loading
    Checks if "config" is specified in:
    - project env
    - system env
    - system props
    If found - treats it as a separate file and merges into the current env.
    
    So basically it makes it easier to keep a separate configuration file
    with secrets per profile and/or depending on environment, for example if
    used without lein

--- environ/src/environ/core.clj
+++ environ/src/environ/core.clj
@@ -23,15 +23,20 @@
        (map (fn [[k v]] [(keywordize k) v]))
        (into {})))
 
-(defn- read-env-file []
-  (let [env-file (io/file ".lein-env")]
+(defn- read-env-file [file-name]
+  (let [env-file (io/file file-name)]
     (if (.exists env-file)
       (into {} (for [[k v] (read-string (slurp env-file))]
                  [(sanitize k) v])))))
 
+(defn- load-subconfig [source-fn]
+  (let [cfg (source-fn)]
+    (if-let [subconf-file (:config cfg)]
+      (merge cfg (read-env-file subconf-file))
+      cfg)))
+
 (def ^{:doc "A map of environment variables."}
   env
-  (merge
-   (read-env-file)
-   (read-system-props)
-   (read-system-env)))
+  (->> [(partial read-env-file ".lein-env") read-system-props read-system-env]
+       (map load-subconfig)
+       (apply merge)))
